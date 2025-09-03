7bb901d85bf5a66059f71e0dd8117a793b917737
ardalangg1373 <dr.galedari1373@gmail.com>
2025-09-03 20:38:33 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from '@/components/ui/sonner';
+import "./App.css";
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
