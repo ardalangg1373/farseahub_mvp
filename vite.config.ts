3f05a25b73a99084e07eb2eb9f17d050bc790433
ardalangg1373 <dr.galedari1373@gmail.com>
2025-09-04 20:08:53 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,3 +11,20 @@ export default defineConfig({
     },
   },
 })
+
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      // بای‌پس ارور: هر جا libNext دیده شد، i18next استفاده شود
+      libNext: "i18next",
+    },
+  },
+});
