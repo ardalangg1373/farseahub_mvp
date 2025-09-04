46234f7758fa8026ffe148b8e346aa9948f40d8e
ardalangg1373 <dr.galedari1373@gmail.com>
2025-09-04 18:00:09 +0330

    Update 55main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+// src/main.tsx
+import React, { Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(<App />);
+// i18n باید قبل از رندر لود شود
+import "./i18n";
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Suspense fallback={<div style={{ padding: 24, color: "#bbb" }}>Loading…</div>}>
+      <App />
+    </Suspense>
+  </React.StrictMode>
+);
