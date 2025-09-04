70f35e551623bc12d823f6f3ccf4f2f05f6f9720
ardalangg1373 <dr.galedari1373@gmail.com>
2025-09-04 20:19:19 +0330

    Update last vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      libNext: "i18next", // بای‌پس ارور قبلی
+      // اگر برای بای‌پس ارور قبلی نیاز داری:
+      libNext: "i18next",
     },
   },
 });
